69dc4892f4995cb035b779a8298ef49865371a11
Luiz Henrique <luizhjramos@outlook.com>
2022-06-10 23:02:28 -0300

    feat(Posts): handling error and loading flux with react-query

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -17,8 +17,16 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data } = useQuery("posts", fetchPosts);
+  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts);
 
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Oops, something went wrong!</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
