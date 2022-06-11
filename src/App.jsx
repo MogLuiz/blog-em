d54d6615ee565e42db3cc4572f57b86ecf3d91d2
Luiz Henrique <luizhjramos@outlook.com>
2022-06-10 21:54:48 -0300

    chore(appProvider): React Query app provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Posts } from "./Posts";
 import "./App.css";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
+   <QueryClientProvider client={queryClient}>
+     <div className="App">
       <h1>Blog Posts</h1>
       <Posts />
     </div>
+   </QueryClientProvider>
   );
 }
 
