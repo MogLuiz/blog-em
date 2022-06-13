c8257284f8d5ca6875083c217edadacf3c78bc28
Luiz Henrique <luizhjramos@outlook.com>
2022-06-13 00:05:29 -0300

    feat(PostDetail): comments data fetching with react-query

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,8 +24,18 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery("comments", () =>
+    fetchComments(post.id)
+  );
+
+  if(isLoading) return <h3>Loading!</h3>
+
+  if(isError) return (
+    <>
+    <h3>Error</h3>
+    <p>{error.toString()}</p>
+    </>
+  )
 
   return (
     <>
