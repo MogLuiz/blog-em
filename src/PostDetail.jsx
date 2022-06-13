4458882bf2e3ae2069f4785805b71d778b121003
Luiz Henrique <luizhjramos@outlook.com>
2022-06-13 00:30:34 -0300

    feat(PostDetail): add useQuery array dependency

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -24,7 +24,7 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  const { data, isLoading, isError, error } = useQuery("comments", () =>
+  const { data, isLoading, isError, error } = useQuery(["comments", post.id], () =>
     fetchComments(post.id)
   );
 
