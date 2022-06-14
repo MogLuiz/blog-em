b8c72a583b3385e56f57b514069a5c027bb6085f
Luiz Henrique <luizhjramos@outlook.com>
2022-06-13 22:10:42 -0300

    feat(Posts): react query data fetching with pagination

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -5,19 +5,19 @@ import { useQuery } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNumber) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, { staleTime: 2000 });
+  const { data, isError, error, isLoading } = useQuery(["posts", currentPage],() => fetchPosts(currentPage), { staleTime: 2000 });
 
   if (isLoading) return <h3>Loading...</h3>;
   if (isError)
