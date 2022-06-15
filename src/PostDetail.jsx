e25da8ddc0fa781728a09dd82f811a20536e9dbf
Luiz Henrique <luizhjramos@outlook.com>
2022-06-14 22:24:16 -0300

    refact(PostDetail): refactoring the correctly update route

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -30,7 +30,7 @@ export function PostDetail({ post }) {
   );
 
   const deleteMutation = useMutation((postId) => deletePost(postId));
-  const updateMutation = useMutation((postId) => updateMutation(postId))
+  const updateMutation = useMutation((postId) => updatePost(postId));
 
   if (isLoading) return <h3>Loading!</h3>;
 
@@ -45,17 +45,30 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button onClick={() => deleteMutation.mutate(post.id)}>
-        Delete
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
       </button>
-      {deleteMutation.isError &&  <p style={{ color: "red" }}>Error deleting the post</p>}
-      {deleteMutation.isLoading && <p style={{ color: "purple" }}>Deleting the post</p>}
-      {deleteMutation.isSuccess && <p style={{ color: "green" }}>Post has been deleted!</p>}
+      {deleteMutation.isError && (
+        <p style={{ color: "red" }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: "purple" }}>Deleting the post</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post has been deleted!</p>
+      )}
 
-      {updateMutation.isError &&  <p style={{ color: "red" }}>Error updating the post</p>}
-      {updateMutation.isLoading && <p style={{ color: "purple" }}>Updating the post</p>}
-      {updateMutation.isSuccess && <p style={{ color: "green" }}>Post has been updated!</p>}
-      <button onClick={() => updateMutation.mutate(post.id)}>Update title</button>
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post has been updated!</p>
+      )}
+
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
